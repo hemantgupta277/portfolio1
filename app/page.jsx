fcd11bf6eb6fb308dd822a92079eb1d2c2424c31
hemantgupta277 <hemantworkspace277@gmail.com>
2024-07-18 22:21:39 +0530

    fixes

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br/> <span className="text-accent">Hemant Gupta</span>
+              Hello I am <br/> <span className="text-accent">Hemant Gupta</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at crafting elegant digital experiences and I am Proficient in various programming languagesand technoligies.
